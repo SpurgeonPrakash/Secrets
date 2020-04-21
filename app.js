180f1ae7906e65a2938c79a2f2a06d60992707da
Spazen's PC <prakashamtara07@gmail.com>
2020-04-21 18:17:19 +0530

    added md5 and bcrypt for password encryption

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+// const bcrypt = require("bcrypt");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+// const saltRounds = 10;
 
 const app = express();
 
@@ -21,7 +24,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -38,21 +41,59 @@ app.get("/register", (req,res) => {
 });
 
 app.post("/register", (req,res) => {
-  const newUser = new User({
-    email:req.body.username,
-    password: req.body.password
-  });
-  newUser.save((err) => {
-    if(!err) {
-      res.render("secrets");
-    } else {
-      console.log(err);
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // password: md5(req.body.password)
+    const newUser = new User({
+      email:req.body.username,
+      password: hash
+    });
+    newUser.save((err) => {
+      if(!err) {
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    });
   });
 });
 
+//password encryption using bcrypt
+// app.post("/register", (req,res) => {
+//   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+//     // password: md5(req.body.password)
+//     const newUser = new User({
+//       email:req.body.username,
+//       password: hash
+//     });
+//     newUser.save((err) => {
+//       if(!err) {
+//         res.render("secrets");
+//       } else {
+//         console.log(err);
+//       }
+//     });
+//   });
+// });
+
+//password encryption using md5
+// app.post("/register", (req,res) => {
+//
+//   const newUser = new User({
+//     email:req.body.username,
+//     password: md5(req.body.password)
+//   });
+//   newUser.save((err) => {
+//     if(!err) {
+//       res.render("secrets");
+//     } else {
+//       console.log(err);
+//     }
+//   });
+// });
+
 app.post("/login", (req,res) => {
   const username = req.body.username;
+  // const password = md5(req.body.password);
   const password = req.body.password;
 
   User.findOne({email:username}, (err, foundUser) => {
@@ -60,13 +101,54 @@ app.post("/login", (req,res) => {
       console.log(err);
     } else {
       if(foundUser) {
-        if(foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result === true) {
+            res.render("secrets");
+          }
+        });
       }
     }
-  })
+  });
 });
+
+//password encryption using bcrypt
+// app.post("/login", (req,res) => {
+//   const username = req.body.username;
+//   // const password = md5(req.body.password);
+//   const password = req.body.password;
+//
+//   User.findOne({email:username}, (err, foundUser) => {
+//     if(err) {
+//       console.log(err);
+//     } else {
+//       if(foundUser) {
+//         bcrypt.compare(password, foundUser.password, function(err, result) {
+//           if(result === true) {
+//             res.render("secrets");
+//           }
+//         });
+//       }
+//     }
+//   });
+// });
+
+//password encryption using md5 hashing
+// app.post("/login", (req,res) => {
+//   const username = req.body.username;
+//   const password = md5(req.body.password);
+//
+//   User.findOne({email:username}, (err, foundUser) => {
+//     if(err) {
+//       console.log(err);
+//     } else {
+//       if(foundUser) {
+//         if(foundUser.password === password) {
+//           res.render("secrets");
+//         }
+//       }
+//     }
+//   });
+// });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
