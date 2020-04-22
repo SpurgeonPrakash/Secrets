c2eefaf295348096b98a6d3f701dbee040a86939
Spazen's PC <prakashamtara07@gmail.com>
2020-04-22 19:33:48 +0530

    add multiple secrets facility to each user and complete full prooject

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
   password: String,
   googleId: String,
   facebookId: String,
-  secret: String
+  secrets: Array
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -117,22 +117,22 @@ app.get("/register", (req,res) => {
 });
 
 app.get("/secrets", (req,res) => {
-  // if(req.isAuthenticated()) {
-  //   res.render("secrets");
-  // } else {
-  //   res.redirect("/login");
-  // }
-  User.find({"secret": {$ne: null}}, (err,foundUsers) => {
-    if(err){
-      console.log(err);
-    } else {
-      if(foundUsers) {
-        res.render("secrets", {usersWithSecrets : foundUsers});
+  if(req.isAuthenticated()) {
+    User.find({"secrets": {$ne: null}}, (err,foundUsers) => {
+      if(err){
+        console.log(err);
       } else {
-        res.render("secrets", {usersWithSecrets: {}})
+        if(foundUsers) {
+          res.render("secrets", {usersWithSecrets : foundUsers});
+        } else {
+          res.render("secrets", {usersWithSecrets: defaultSecret});
+        }
       }
-    }
-  });
+    });
+  } else {
+    res.redirect("/login");
+  }
+
 });
 
 app.get("/logout", (req,res) => {
@@ -185,13 +185,13 @@ app.post("/login", (req,res) => {
 app.post("/submit", (req,res) => {
   const submittedSecret = req.body.secret;
 
-  console.log(req.user);
+  // console.log(req.user);
   User.findById(req.user.id, (err,foundUser) => {
     if(err) {
       console.log(err);
     } else {
       if(foundUser) {
-        foundUser.secret = submittedSecret;
+        foundUser.secrets.push(submittedSecret);
         foundUser.save(() => {
           res.redirect("/secrets");
         });
